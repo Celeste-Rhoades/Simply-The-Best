3416336f9e998803806a19d6171814fb0f38e10e
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-05-15 21:30:46 -0600

    added notifications

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,15 @@
 import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  followUnfollowUser,
+  getUserProfile,
+  updateUser,
+} from "../controller/user.controller.js";
 
 const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update", protectRoute, updateUser);
 
 export default router;
