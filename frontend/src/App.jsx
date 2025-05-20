e673490cc2bb8cb25a941d2f1044f32bc7036197
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-05-20 12:10:37 -0600

    added react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignInPage from "./pages/auth/SignInPage"
 import SignUpPage from "./pages/auth/SignUpPage"
 
+
 const App = () => {
   return (
-    <BrowserRouter>
+    <>
+  
       <Routes>
         <Route path="/" element={<SignInPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
+       
       </Routes>
-    </BrowserRouter>
+    
+    </>
   )
 }
 
