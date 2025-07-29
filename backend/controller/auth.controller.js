185e0614c959e8a3ca20fae71f179c312c21c435
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-07-28 20:33:06 -0600

    change a route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -87,7 +87,7 @@ export const logout = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-export const getMe = async (req, res) => {
+export const getProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select("-password");
     res.status(200).json(user);
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { login, logout, signup, getMe } from "../controller/auth.controller.js";
+import {
+  login,
+  logout,
+  signup,
+  getProfile,
+} from "../controller/auth.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 import { getUserRecommendations } from "../controller/user.controller.js";
 
 const router = express.Router();
 
-router.get("/me", protectRoute, getMe);
+router.get("/myProfile", protectRoute, getprofile);
 router.get("/:username/recommendations", protectRoute, getUserRecommendations);
 
 router.post("/signup", signup);
