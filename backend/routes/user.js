ee7d3d2badad8e6a4d73dfb8f21b4997e67c8569
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-08-31 10:36:44 -0600

    updated routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,14 +5,22 @@ import {
   getUserProfile,
   updateUser,
   searchUsers,
+  sendFriendRequest,
+  accceptFriendRequest,
+  declineFriendRequest,
 } from "../controller/user.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/search", protectRoute, searchUsers);
+router.get("/friends", protectRoute, friendsList);
+router.get("/friend-requests/pending", protectRoute, friendRequestpending);
 
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 router.post("/update", protectRoute, updateUser);
+router.post("/friend-request/send/:id", protectRoute, sendFriendRequest);
+router.post("/friend-request/accept/:id", protectRoute, accceptFriendRequest);
+router.post("/friend-request/decline/:id", protectRoute, declineFriendRequest);
 
 export default router;
