ab83d6ca4a9e074a3070fcc0fb15f1572dcf87de
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-11-21 17:29:39 -0700

    updated for cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,26 @@ const app = express();
 
 const PORT = process.env.PORT || 5288;
 
-const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://simplytheebest.netlify.app",
+  process.env.FRONTEND_URL,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(
   cors({
